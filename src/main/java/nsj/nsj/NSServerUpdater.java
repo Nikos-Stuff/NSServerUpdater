c3f004adebdd73c88c94e47b42d9afbc5c2fe9ed
_Nik0__ <maciek.f137@gmail.com>
2024-06-09 04:08:59 +0200

    Initial commit

---
+++ src/main/java/nsj/nsj/NSServerUpdater.java
@@ -0,0 +1,47 @@
+package nsj.nsj;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class NSServerUpdater extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Server Updater Enabled...");
+
+        // Get the path to the root directory of the server
+        Path serverDir = Paths.get("").toAbsolutePath();
+
+        String downloadUrl = "https://api.nikostuff.com/v1/latest_jar?download";
+        String fileName = serverDir.resolve("server_u.jar").toString();
+
+        try {
+            downloadFile(downloadUrl, fileName);
+            getLogger().info("Download completed. File saved as: " + fileName);
+        } catch (IOException e) {
+            getLogger().warning("Failed to download the file: " + e.getMessage());
+        }
+    }
+
+    private void downloadFile(String url, String fileName) throws IOException {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+        }
+    }
+
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
